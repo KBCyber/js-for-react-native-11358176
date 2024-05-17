14f934d477366a2e352f37d1e6a7d67901c24256
KBCyber <aninyung@st.ug.edu.gh>
2024-05-17 17:12:39 +0000

    First Task completed

---
+++ arrayManipulation.js
@@ -0,0 +1,17 @@
+function processArray(arr) {
+    let result = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            result.push(arr[i] * arr[i]);
+        } else {
+            result.push(arr[i] * 3);
+        }
+    }
+    
+    return result;
+}
+
+const inputArray = [1, 2, 3, 4, 5];
+const newArray = processArray(inputArray);
+console.log(newArray);
