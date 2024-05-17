93b9aa8b061542572a137e7348bf24cbba309b2b
KBCyber <aninyung@st.ug.edu.gh>
2024-05-17 17:55:52 +0000

    Second task completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,5 +1,3 @@
-// First Task
-
 function processArray(arr) {
     let result = [];
     
@@ -13,7 +11,28 @@ function processArray(arr) {
     
     return result;
 }
+const processedArray = processArray(inputArray);
+console.log(processedArray);
 
-const inputArray = [1, 2, 3, 4, 5];
-const newArray = processArray(inputArray);
-console.log(newArray);
+
+function formatArrayStrings(strings, numbers) {
+    if (strings.length !== numbers.length) {
+        throw new Error("Arrays must have the same length.");
+    }
+
+    let formattedStrings = [];
+
+    for (let i = 0; i < strings.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            formattedStrings.push(strings[i].toUpperCase());
+        } else {
+            formattedStrings.push(strings[i].toLowerCase());
+        }
+    }
+
+    return formattedStrings;
+}
+
+const strings = ["hello", "world", "JavaScript", "Node.js"];
+const numbers = [1, 2, 3, 4];
+const formattedStrings = formatArrayStrings(strings, numbers);
